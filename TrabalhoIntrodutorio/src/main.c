21d37cd3e4bfa546ff91a05adbedc86130c906c8
Andrey <andrey.cortez12@gmail.com>
2024-04-10 15:20:54 -0300

    sometimes sometimes

--- TrabalhoIntrodutorio/src/main.c
+++ TrabalhoIntrodutorio/src/main.c
@@ -1,4 +1,15 @@
 #include <csv.h>
+#include <dbms.h>
+
+void print_bytes(const void *ptr, size_t size)
+{
+    const unsigned char *bytes = ptr;
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("%02X ", bytes[i]);
+    }
+    printf("\n");
+}
 
 int main()
 {
@@ -15,6 +26,20 @@ int main()
     csv_print_head(hand);
     // printf("%s", hand->data[0][0]);
 
+    const char *data_to_format[] = {"12", "12.0", "Tamanho arbitrario"};
+    const char *format_string = "%d %f %s";
+    
+    int tam = format_len(format_string, data_to_format);
+
+    printf("\ntamanho dos dados: %d\n", tam);
+    void *formated = format_data(format_string, data_to_format);
+    print_bytes(formated, tam);
+
+    printf("%d %f ", *((int *)formated), *((float *)(formated + sizeof(int))));
+    // for (int i)
+    printf("%s\n", (char *)(formated + 12));
+
+    printf("testes encerrados\n");
     // Fechar o arquivo
     fclose(file);
 
